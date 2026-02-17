15b096770c415a821b823e9090f84b2081370c4d
emsfsilva <ems.fsilva.pe@gmail.com>
2026-02-17 16:51:55 -0300

    ajuste no codigo

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
 import { LoginPayload } from '../dtos/loginPayload.dto';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET || 'seu-secret',
-    });
+    } as StrategyOptions);
   }
 
   async validate(payload: LoginPayload): Promise<LoginPayload> {
-    // Aqui você pode adicionar validações extras se quiser
     return payload;
   }
 }
